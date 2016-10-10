9b85a6071342b3a7af0e1cf76477000fdcd87a98
shubham0704 <shubham.bhardwaj2015@vit.ac.in>
2016-10-10 20:15:03 +0530

    enabled crossDomain for api access

--- controllers/chat.py
+++ controllers/chat.py
@@ -1,27 +1,36 @@
 from modules import *
 
 class QuestionHandler(RequestHandler):
-	'''when user scrolls down to the FAQ section an ajax request to the get method for welcoming the user
-	   also when te user scolls then the text welcoming the user must be dynamically deleted
-	'''
-	@coroutine
-	@removeslash
-	def get(self):
-		status_code=True
-		emailId=self.get_argument('email')
-		result=yield db.users.find_one({'emailId':emailId})
-		if result:			
-			self.write('{status:%s,name:%s}'%(status_code,result['name']))
-		else:
-			self.write('{status:%s}'%(status_code))	
-	@coroutine
-	@removeslash
-	def post(self):
-		#self.write("<p>What is your name:</p>")
-		question=self.get_argument('question');			
-		response = chatbot.get_response(question)
-		print 'response',response	
-		'''
-		Print the response based upon the training
-		'''
-		self.write('{response:%s}'%response)
+    '''when user scrolls down to the FAQ section an ajax request to the get method for welcoming the user
+       also when te user scolls then the text welcoming the user must be dynamically deleted
+    '''
+    '''@coroutine
+    @removeslash
+    def get(self):
+        status_code=True
+        emailId=self.get_argument('email')
+        result=yield db.users.find_one({'emailId':emailId})
+        if result:          
+            self.write('{status:%s,name:%s}'%(status_code,result['name']))
+        else:
+            self.write('{status:%s}'%(status_code)) 
+    '''
+    def set_default_headers(self):
+        print "setting headers!!!"
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+    
+    
+    @coroutine
+    @removeslash
+    def get(self):
+        #self.write("<p>What is your name:</p>")
+        question=self.get_argument('question');         
+        print question
+        response = chatbot.get_response(question)
+        print 'response',response   
+        '''
+        Print the response based upon the training
+        '''
+        self.write('{"status":"success","data":%s}'%response)
