c6da6c7c9b4b0bfe49c6e219b5156b4e331a6f11
shubham0704 <shubham.bhardwaj2015@vit.ac.in>
2016-10-12 22:07:40 +0530

    minor changes

--- controllers/chat.py
+++ controllers/chat.py
@@ -8,7 +8,7 @@ class QuestionHandler(RequestHandler):
     def set_default_headers(self):
         print "setting headers!!!"
         self.set_header("Access-Control-Allow-Origin", "*")
-        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        #self.set_header("Access-Control-Allow-Headers", "x-requested-with")
         self.set_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
     
     
